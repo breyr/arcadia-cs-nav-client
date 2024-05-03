6d91a2278286e6dd46e9a8051ffb43ba9bb8d076
breyr <breyr2021@gmail.com>
2024-05-03 02:39:42 -0400

    chore: Update npm dependencies and add Svelte Flow graph component to track page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,5 @@
 <div class="text-center mt-60">
     <h1 class="text-5xl font-bold">About This</h1>
     <p class="py-6">description about what this project is and who made it</p>
+    <span>graphs created by <a href="https://svelteflow.dev/" target="_blank" class="link">svelteflow</a>!</span>
 </div>
--- src/routes/tracks/[title=track]/+page.svelte
+++ src/routes/tracks/[title=track]/+page.svelte
@@ -1,10 +1,52 @@
 <script lang="ts">
     import { page } from '$app/stores';
+    import {
+    	Controls,
+    	SvelteFlow
+    } from '@xyflow/svelte';
+    import '@xyflow/svelte/dist/style.css';
+    import { writable } from 'svelte/store';
+
     export let data;
+
+    // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+    const nodes = writable([
+        {
+            id: '1',
+            type: 'input',
+            data: { label: 'Input Node' },
+            position: { x: 0, y: 0 }
+        },
+        {
+            id: '2',
+            type: 'default',
+            data: { label: 'Node' },
+            position: { x: 0, y: 150 }
+        }
+    ]);
+    
+    // same for edges
+    const edges = writable([
+        {
+            id: '1-2',
+            type: 'default',
+            source: '1',
+            target: '2',
+            label: 'Edge Text'
+        }
+    ]);
+    
+    const snapGrid: [number, number] = [25, 25];
+    const proOptions = { hideAttribution: true };
+
     let completed = 20; // replace with your actual progress value
+    let activeChart = 'recommended';
+    function setActiveChart(chart: 'recommended' | 'arcadia' | 'extra') {
+        activeChart = chart;
+    }
 </script>
 
-<div class="container mx-4">
+<div class="container mx-auto flex flex-col">
     <!-- breadcrumbs -->
     <div class="text-sm breadcrumbs">
         <ul>
@@ -14,11 +56,49 @@
     </div>
 
     <!-- title and description -->
-    <div class="text-white">
+    <div class="text-white mb-3">
         <h1 class="text-4xl font-semibold">{data.track.title}</h1>
         <h3 class="text-xl text-gray-300 font-light mb-2">{data.track.description}</h3>
         <span class="badge badge-neutral">Last Updated: {data.track.updatedDate.toLocaleDateString()}</span>
     </div>
+
+    <div role="tablist" class="tabs tabs-boxed mb-2">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <a role="tab" class={`tab ${activeChart === 'recommended' ? 'tab-active' : ''}`} on:click={() => setActiveChart('recommended')}>Recommended Learning</a>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <a role="tab" class={`tab ${activeChart === 'arcadia' ? 'tab-active' : ''}`} on:click={() => setActiveChart('arcadia')}>Arcadia Courses</a>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <a role="tab" class={`tab ${activeChart === 'extra' ? 'tab-active' : ''}`} on:click={() => setActiveChart('extra')}>Extra Resources</a>
+    </div>
+
+    <!-- track graph -->
+    <!-- have to make hidden because nodes will disappear if inside if statement -->
+    <div style="height:70vh; width: 100%;" hidden={activeChart !== 'recommended'}>
+        <SvelteFlow
+            {nodes}
+            {edges}
+            {snapGrid}
+            fitView
+            style="background: transparent;"
+            proOptions={proOptions}
+            on:nodeclick={(event) => console.log('on node click', event.detail.node)}
+        >
+            <Controls />
+        </SvelteFlow>
+    </div>
+    {#if activeChart === 'arcadia'}
+    <!-- TODO make this a seperate flow component so states don't mix -->
+        <p>arcadia courses graph </p>
+    {:else if activeChart === 'extra'}
+        <p>Certs</p>
+    {/if}
+
 
     <!-- Progress bar -->
     <div class="absolute bottom-0 right-0 mx-4 my-4 flex flex-col items-end glass pb-3 px-3 py-1 rounded">
@@ -29,7 +109,6 @@
 
 <style>
     .glass {
-        /* From https://css.glass */
         background: rgba(255, 255, 255, 0.05);
         border-radius: 16px;
         box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
